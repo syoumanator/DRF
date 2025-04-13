93e0fe01a14c0bbf8fdf4480579958c21fc8a65c
Alik <alik220201@gmail.com>
2025-04-13 22:47:23 +0700

    DEFAULT_PERMISSION_CLASSES

--- config/settings.py
+++ config/settings.py
@@ -60,8 +60,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+        'rest_framework_simplejwt.authentication.JWTAuthentication',),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ]
 }
 
 DATABASES = {
