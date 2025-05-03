0afbbcaf49ae7107ad2b7222d16679c7fc4c791f
Alik <alik220201@gmail.com>
2025-05-03 16:35:07 +0700

    add task "course update"

--- education/tasks.py
+++ education/tasks.py
@@ -1,4 +1,7 @@
 from celery import shared_task
+from education.models import Subscription
+from django.core.mail import send_mail
+from config import settings
 
 
 @shared_task
@@ -7,5 +10,16 @@ def last_visit():
 
 
 @shared_task
-def update_course_mail():
-    pass
+def update_course_mail(course_id):
+    subscription_course = Subscription.objects.filter(course=course_id)
+    sub_list = [sub.user.email for sub in subscription_course]
+    if sub_list:
+        subject = f"Изменение курса {subscription_course.first().course.title}"
+        message = f"Курс {subscription_course.first().course.title} изменен."
+        send_mail(
+            subject,
+            message,
+            settings.DEFAULT_FROM_EMAIL,
+            sub_list,
+            fail_silently=False,
+        )
